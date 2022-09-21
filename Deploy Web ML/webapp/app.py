ddb7486f05c1b2bf7d4e1ad2072d04c26079b1a5
Vinícius Nunes Rebeque <viniciusrebeque96@gmail.com>
2022-09-21 17:18:34 -0300

    Novas alterações no modo de operação do modelo e deploy

--- Deploy Web ML/webapp/app.py
+++ Deploy Web ML/webapp/app.py
@@ -3,7 +3,7 @@ import pickle
 import pandas as pd
 
 # Lê modelo usando pickle
-with open(f'model/modelo_final_arquivo.pkl', 'rb') as f:
+with open(f'model/modelo_final_v2.pkl', 'rb') as f:
     model = pickle.load(f)
 
 app = flask.Flask(__name__, template_folder='templates')
@@ -12,17 +12,16 @@ app = flask.Flask(__name__, template_folder='templates')
 @app.route('/', methods=['GET', 'POST'])
 def main():
     if flask.request.method == 'GET':
-        return flask.render_template('main.html')
+        return (flask.render_template('main.html'))
 
     if flask.request.method == 'POST':
         Pclass = flask.request.form['Pclass']
-        Sex_female = flask.request.form['Sex_female']
-        Sex_male = flask.request.form['Sex_male']
+        Sex = flask.request.form['Sex']
 
-        input_variables = pd.DataFrame([[Pclass, Sex_female, Sex_male]], columns=['Pclass', 'Sex_female', 'Sex_male'], dtype=float)
+        input_variables = pd.DataFrame([[Pclass, Sex]], columns=['Pclass', 'Sex']).astype('int64')
         prediction = model.predict(input_variables)[0]
 
-        return flask.render_template('main.html', original_input={'Classe de Embarque':Pclass, 'Sex_female':Sex_female, 'Sex_male':Sex_male}, result=prediction)
+        return flask.render_template('main.html', original_input={'Classe de Embarque':Pclass, 'Sexo':Sex}, result=prediction)
 
 
 if __name__ == '__main__':
