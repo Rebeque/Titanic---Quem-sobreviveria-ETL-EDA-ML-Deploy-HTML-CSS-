1817cd642e5f0773858dcf1a5b26ed856462558e
Vinícius Nunes Rebeque <viniciusrebeque96@gmail.com>
2022-09-27 17:14:12 -0300

    Nova feature adicionada ao deploy com sucesso

--- Deploy Web ML/webapp/app.py
+++ Deploy Web ML/webapp/app.py
@@ -3,7 +3,7 @@ import pickle
 import pandas as pd
 
 # Lê modelo usando pickle
-with open(f'model/modelo_final_v3.pkl', 'rb') as f:
+with open(f'model/modelo_final_v4.pkl', 'rb') as f:
     model = pickle.load(f)
 
 app = flask.Flask(__name__, template_folder='templates')
@@ -17,15 +17,16 @@ def main():
     if flask.request.method == 'POST':
         Pclass = flask.request.form['Pclass']
         Sex = flask.request.form['Sex']
+        Embarked = flask.request.form['Embarked']
 
-        input_variables = pd.DataFrame([[Pclass, Sex]], columns=['Pclass', 'Sex']).astype('int64')
+        input_variables = pd.DataFrame([[Pclass, Sex, Embarked]], columns=['Pclass', 'Sex', 'Embarked']).astype('int64')
         prediction = model.predict(input_variables)[0]
 
     if prediction == 0:
-        return flask.render_template('main.html', original_input={'Classe de Embarque':Pclass, 'Sexo':Sex}, result=('Morreu'))
+        return flask.render_template('main.html', original_input={'Classe de Embarque':Pclass, 'Sexo':Sex, 'Estação de Embarque':Embarked}, result=('Morreu'))
 
     if prediction == 1:
-        return flask.render_template('main.html', original_input={'Classe de Embarque':Pclass, 'Sexo':Sex}, result=('Sobreviveu'))
+        return flask.render_template('main.html', original_input={'Classe de Embarque':Pclass, 'Sexo':Sex, 'Estação de Embarque':Embarked}, result=('Sobreviveu'))
 
 
 if __name__ == '__main__':
